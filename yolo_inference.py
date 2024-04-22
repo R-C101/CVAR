671101961dfe4b475bbc9b1fdf0e115816314d37
Rishit Chugh <rishitchugh@gmail.com>
2024-04-22 22:06:34 +0530

    custom model added

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x.pt')  
+model = YOLO('models/best.pt')  
 
-results = model.predict('input_videos/08fd33_4.mp4', save = True)
+results = model.predict('input_videos/08fd33_4.mp4', save = True, save_dir = 'output_videos')
 print(results[0])
 for box in results[0].boxes:
     print(box)
