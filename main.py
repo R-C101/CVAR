cabbd2da9d49232a910f7aa76e203f088d0cbdf4
Rishit Chugh <rishitchugh@gmail.com>
2024-04-23 21:52:25 +0530

    added annotations

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ def main():
     tracker = Tracker('models/bestest.pt')
     tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
     
+    #draw output
+    #draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames,tracks)
+    save_video(output_video_frames, 'output_videos/outputvideo_with_tracks.avi')
+    
 if __name__ == '__main__':
     main()
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -2,6 +2,10 @@ from ultralytics import YOLO
 import supervision as sv
 import pickle
 import os
+import cv2
+import sys
+sys.path.append('../')
+from utils import get_center_of_bbox, get_bbox_width
 class Tracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
@@ -66,3 +70,66 @@ class Tracker:
             with open(stub_path, 'wb') as f:
                 pickle.dump(tracks, f)
         return tracks
+
+    def draw_ellipse(self,frame,bbox,color,track_id=None):
+        y2 = int(bbox[3])
+        
+        x_center, _ = get_center_of_bbox(bbox)
+        width = get_bbox_width(bbox)
+        
+        cv2.ellipse(frame,
+                    center=(x_center, y2),
+                    axes=(int(width), int(0.35*width)),
+                    angle = 0.0,
+                    startAngle=-45,
+                    endAngle=235,
+                    color=color,
+                    thickness=2,
+                    lineType=cv2.LINE_4)
+        
+        rectange_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - rectange_width // 2
+        x2_rect = x_center + rectange_width // 2
+        y1_rect = (y2-rectangle_height//2) + 15
+        y2_rect = (y2+rectangle_height//2) + 15
+        if track_id is not None:
+            cv2.rectangle(
+                 frame,
+                 (int(x1_rect), int(y1_rect)),
+                 (int(x2_rect), int(y2_rect)),
+                 color,
+                 cv2.FILLED
+             )
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -=10
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_text), int(y1_rect + 15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 0, 0),
+                2
+                
+            )
+        
+        return frame
+            
+    def draw_annotations(self, video_frames,tracks):
+        output_video_frames = []
+        for frame_num, frame in enumerate(video_frames):
+            frame = frame.copy()
+            player_dict = tracks['players'][frame_num]
+            referee_dict = tracks['referees'][frame_num]
+            ball_dict = tracks['ball'][frame_num]
+            
+            for track_id, player in player_dict.items():
+                frame = self.draw_ellipse(frame, player['bbox'], (0,255,0), track_id)
+            
+            for _, referee in referee_dict.items():
+                frame = self.draw_ellipse(frame, referee['bbox'], (0,255,255))
+                
+            output_video_frames.append(frame)
+        return output_video_frames
