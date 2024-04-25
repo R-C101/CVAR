89ccd38ffb0bb877e25c5b00497131cf147a20b4
Rishit Chugh <rishitchugh@gmail.com>
2024-04-25 20:53:00 +0530

    added distance and speed estimation

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 import numpy as np
 from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
     save_video(video_frames, 'output_videos/outputvideo.avi')
@@ -15,9 +17,16 @@ def main():
     camera_movement_estimator = CameraMovementEstimator(video_frames[0])
     camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,read_from_stub=True,stub_path='stubs/camera_movement_stub.pkl')
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
+    
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
     #interpolate ball positions
     
     tracks['ball'] = tracker.interpolate_ball_positions(tracks['ball'])
+    
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
+    
     team_assigner = TeamAssigner()
     team_assigner.assign_team_color(video_frames[0], 
                                     tracks['players'][0])
@@ -57,6 +66,8 @@ def main():
     #draw object tracks
     output_video_frames = tracker.draw_annotations(video_frames,tracks,team_ball_control)
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
+    
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,tracks)
     save_video(output_video_frames, 'output_videos/outputvideo_with_tracks.avi')
     
 
