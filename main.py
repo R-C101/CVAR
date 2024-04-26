8710f0ff9e71b913f286507748899dde3a0338f0
Rishit Chugh <rishitchugh@gmail.com>
2024-04-26 13:42:59 +0530

    output_video

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from camera_movement_estimator import CameraMovementEstimator
 from view_transformer import ViewTransformer
 from speed_and_distance_estimator import SpeedAndDistance_Estimator
 def main():
-    video_frames = read_video('input_videos/08fd33_4.mp4')
-    save_video(video_frames, 'output_videos/outputvideo.avi')
+    video_frames = read_video('input_videos/ankaramessi.mp4')
+    # save_video(video_frames, 'output_videos/outputvideo.avi')
     tracker = Tracker('models/bestest.pt')
-    tracks = tracker.get_object_tracks(video_frames,read_from_stub=True,stub_path='stubs/track_stubs.pkl')
+    tracks = tracker.get_object_tracks(video_frames,read_from_stub=False,stub_path='stubs/track_stubs.pkl')
     
     tracker.add_position_to_tracks(tracks)
     camera_movement_estimator = CameraMovementEstimator(video_frames[0])
-    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,read_from_stub=True,stub_path='stubs/camera_movement_stub.pkl')
+    camera_movement_per_frame = camera_movement_estimator.get_camera_movement(video_frames,read_from_stub=False,stub_path='stubs/camera_movement_stub.pkl')
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks,camera_movement_per_frame)
     
     view_transformer = ViewTransformer()
@@ -68,7 +68,7 @@ def main():
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames,camera_movement_per_frame)
     
     speed_and_distance_estimator.draw_speed_and_distance(output_video_frames,tracks)
-    save_video(output_video_frames, 'output_videos/outputvideo_with_tracks.avi')
+    save_video(output_video_frames, 'output_videos/ankaramessi.avi')
     
 
     
